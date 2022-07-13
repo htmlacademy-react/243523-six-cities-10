651cde050a5e3acd77c82aef8b5f8cf3c8656974
PechurinaLena <www.waaay.net@gmail.com>
2022-07-13 19:13:37 +0300

    Added routes to the app component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,49 @@
 import {FC} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import Login from 'pages/login';
 import Main from 'pages/main';
+import Room from 'pages/room';
+import Favorites from 'pages/favorites';
+import NotFound from 'pages/not-found';
+import {AppRoute, AuthorisationStatus} from 'types/const';
+
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   rentalAmount: number,
 }
 
-export const App: FC<AppScreenProps> = ({rentalAmount}) => <Main rentalAmount={rentalAmount}/>;
+export const App: FC<AppScreenProps> = ({rentalAmount}) => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Main rentalAmount={rentalAmount}/>}/>
+      <Route
+        path={AppRoute.Root}
+        element={<Main rentalAmount={rentalAmount}/>}
+      />
+      <Route path={AppRoute.Room} element={<Room/>}/>
+      <Route path='offer' element={<Room/>}>
+        <Route path=':id' element={<Room/>}/>
+      </Route>
+      <Route
+        path={AppRoute.Favorites}
+        element={
+          <PrivateRoute authorisationStatus={AuthorisationStatus.NotAuth}>
+            <Favorites/>
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={AppRoute.Login}
+        element={<Login/>}
+      />
+      <Route
+        path="*"
+        element={<NotFound/>}
+      />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
