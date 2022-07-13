d396f4a82fce985bb94e1e9b3464e0c1ff60df2f
PechurinaLena <www.waaay.net@gmail.com>
2022-07-13 19:12:04 +0300

    Added PrivateRoute

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+import {Navigate} from 'react-router-dom';
+
+import {AppRoute, AuthorisationStatus} from '../../types/const';
+
+type PrivateRouteProps = {
+  authorisationStatus: AuthorisationStatus,
+  children: JSX.Element
+}
+
+export const PrivateRoute: FC<PrivateRouteProps> = ({authorisationStatus, children}) =>
+  authorisationStatus === AuthorisationStatus.Auth ?
+    children :
+    <Navigate to={AppRoute.Login}/>;
+
+export default PrivateRoute;
