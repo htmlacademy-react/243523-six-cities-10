c95bee0e4ad596915b87c610aa600b685b30f8eb
PechurinaLena <www.waaay.net@gmail.com>
2022-07-20 18:49:23 +0300

    Fixed review types

--- project/src/types/reviews.ts
+++ project/src/types/reviews.ts
@@ -1,13 +1,3 @@
-export type Reviews = {
-  id: number,
-  name: string,
-  image: string,
-  description: string,
-  price: number,
-  currency: string,
-  status?: string,
-};
-
 export type Review = {
   id: number,
   name: string,
