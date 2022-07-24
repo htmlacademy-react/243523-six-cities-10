3289abc3917cfc06aefebf679173f9ef7645277d
PechurinaLena <www.waaay.net@gmail.com>
2022-07-25 01:50:33 +0300

    Added cities mocks

---
+++ project/src/mocks/cities.ts
@@ -0,0 +1,52 @@
+import {City} from 'types/offers';
+
+export const cities: City[] = [
+  {
+    title: 'Amsterdam',
+    location: {
+      latitude: 52.371807,
+      longitude: 4.896029,
+      zoom: 3
+    }
+  },
+  {
+    title: 'Brussels',
+    location: {
+      latitude: 50.8503396,
+      longitude: 4.3517103,
+      zoom: 3
+    }
+  },
+  {
+    title: 'Cologne',
+    location: {
+      latitude: 50.933594,
+      longitude: 6.961899,
+      zoom: 3
+    }
+  },
+  {
+    title: 'Paris',
+    location: {
+      latitude: 48.864716,
+      longitude: 2.349014,
+      zoom: 3
+    }
+  },
+  {
+    title: 'Hamburg',
+    location: {
+      latitude: 53.551086,
+      longitude: 9.993682,
+      zoom: 3
+    }
+  },
+  {
+    title: 'Dusseldorf',
+    location: {
+      latitude: 51.233334,
+      longitude: 6.783333,
+      zoom: 3
+    }
+  }
+];
